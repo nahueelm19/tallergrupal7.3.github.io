162f49f504c7321c24f1bd9a00f16f811f960725
nahueelm19 <nahuelm15@hotmail.com>
2025-08-14 08:42:56 -0300

    modifiqué código script

--- js/validacion.js
+++ js/validacion.js
@@ -9,34 +9,32 @@ function showAlertError() {
 document.getElementById("registroForm").addEventListener("submit", function(event) {
 event.preventDefault(); // No manda el formulario todavía
 
-let nombre = document.getElementById("nombre").value.trim();
-let email = document.getElementById("email").value.trim();
-let password = document.getElementById("password").value;
-let repeatPassword = document.getElementById("repeatPassword").value;
+let nombre = document.getElementById("nombre").value;
+let apellido = document.getElementById("apellido").value;
+let email = document.getElementById("email").value;
+let password = document.getElementById("password1").value;
+let repeatPassword = document.getElementById("password2").value;
 let terminos = document.getElementById("terminos").checked;
 
 // Ningún campo vacío
-if (nombre === "" || email === "" || password === "" || repeatPassword === "") {
+if (nombre === "" || apellido === "" || email === "" || password === "" || repeatPassword === "") {
 showAlertError();
-return;
 }
 
 // Contraseña mínimo 6 caracteres
 if (password.length < 6) {
 showAlertError();
-return;
 }
 
 // Contraseñas iguales
 if (password !== repeatPassword) {
 showAlertError();
-return;
+
 }
 
 // Checkbox de términos marcado
 if (!terminos) {
 showAlertError();
-return;
 }
 
 // Si todo está bien
