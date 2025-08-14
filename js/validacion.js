21d6262cee4b70588b87548c631099275e7380bb
nahueelm19 <nahuelm15@hotmail.com>
2025-08-14 09:07:23 -0300

    modifiqué código script

--- js/validacion.js
+++ js/validacion.js
@@ -6,8 +6,8 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-document.getElementById("registroForm").addEventListener("submit", function(event) {
-event.preventDefault(); // No manda el formulario todavía
+document.getElementById("regBtn").addEventListener("click", function(event) {
+
 
 let nombre = document.getElementById("nombre").value;
 let apellido = document.getElementById("apellido").value;
@@ -19,22 +19,25 @@ let terminos = document.getElementById("terminos").checked;
 // Ningún campo vacío
 if (nombre === "" || apellido === "" || email === "" || password === "" || repeatPassword === "") {
 showAlertError();
+return;
 }
 
 // Contraseña mínimo 6 caracteres
 if (password.length < 6) {
 showAlertError();
+return;
 }
 
 // Contraseñas iguales
 if (password !== repeatPassword) {
 showAlertError();
-
+return;
 }
 
 // Checkbox de términos marcado
 if (!terminos) {
 showAlertError();
+return;
 }
 
 // Si todo está bien
